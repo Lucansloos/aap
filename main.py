d0acd19d4a912a6ab4f38a7cd87c6d5038ebd1dc
Lucas Teunisse <>
2021-06-06 17:40:19 +0000

    levens en geraden letters

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 print('Zin in galgje?')
 words = ["informatica","informatiekunde","spelletje","aardigheidje","scholier","fotografie","waardebepaling","specialiteit","verzekering","universiteit","heesterperk"]
 GeheimWoord = words[random.randrange(0, len(words))]
+GeradenLetters = []
+levens = 10
+
+def checkWin():
+    for i in range(len(GeheimWoord)):
+        if not GeheimWoord[i] in GeradenLetters:
+            return(False)
+    return(True)
+
+def renderWoord():
+        for a in range(len(GeheimWoord)):
+            if GeheimWoord[a] in GeradenLetters:
+                print(GeheimWoord[a], '', end='')
+            else:
+                print('? ', end='')
+        print('')
+        print('" ' * len(GeheimWoord))
+
+renderWoord()
