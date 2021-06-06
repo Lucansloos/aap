006b3e90c9f293f839c485473b5c82c13046a877
Lucas Teunisse <>
2021-06-06 17:38:01 +0000

    woorden toegevoegd

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
 print('Zin in galgje?')
+words = ["informatica","informatiekunde","spelletje","aardigheidje","scholier","fotografie","waardebepaling","specialiteit","verzekering","universiteit","heesterperk"]
+GeheimWoord = words[random.randrange(0, len(words))]
